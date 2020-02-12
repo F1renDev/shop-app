a54a70fe6f987ad2bf232b30479454cda77c2211
F1renDev <F1renishere@gmail.com>
2020-02-12 17:32:23 +0300

    Added new pages and controllers for them

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const router = express.Router();
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 //Handling the GET request to "/admin/add-product"
-router.get("/add-product", productsController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
 
 //Handling the POST request to "/admin/add-product"
-router.post("/add-product", productsController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
+
+router.get("/products", adminController.getProducts);
 
 module.exports = router;
