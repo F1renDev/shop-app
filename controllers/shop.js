f8bd9304d5a94d3787e9e46288bfccb699ff7962
F1renDev <F1renishere@gmail.com>
2020-03-17 12:37:28 +0300

    Added pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,16 +6,38 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
-  Product.find()
-    .then(products => {
-      // console.log(products);
-      res.render("shop/product-list", {
-        prods: products,
-        pageTitle: "All Products",
-        path: "/products"
-      });
-    })
+ //Getting the page number from query params
+ const page = +req.query.page || 1;
+ let totalItems;
+
+ Product.find()
+   .countDocuments()
+   .then(numberOfProducts => {
+     totalItems = numberOfProducts;
+     return (
+       Product.find()
+         //Skipping the items that should be on previous page
+         //and limiting the quantity of retrieved items
+         .skip((page - 1) * ITEMS_PER_PAGE)
+         .limit(ITEMS_PER_PAGE)
+     );
+   })
+   .then(products => {
+     res.render("shop/product-list", {
+       prods: products,
+       pageTitle: "All Products",
+       path: "/products",
+       currentPage: page,
+       hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+       hasPreviousPage: page > 1,
+       nextPage: page + 1,
+       previousPage: page - 1,
+       lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+     });
+   })
     .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
@@ -45,12 +67,33 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  //Getting the page number from query params
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numberOfProducts => {
+      totalItems = numberOfProducts;
+      return (
+        Product.find()
+          //Skipping the items that should be on previous page
+          //and limiting the quantity of retrieved items
+          .skip((page - 1) * ITEMS_PER_PAGE)
+          .limit(ITEMS_PER_PAGE)
+      );
+    })
     .then(products => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
-        path: "/"
+        path: "/",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
